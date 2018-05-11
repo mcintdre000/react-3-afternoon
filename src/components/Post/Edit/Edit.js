bd4a2a44d2f63a97e7421e88d0342f70179897f3
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-05-10 21:31:45 -0700

    First commit - completed except for the search bar

--- src/components/Post/Edit/Edit.js
+++ src/components/Post/Edit/Edit.js
@@ -20,13 +20,18 @@ export default class Edit extends Component {
   }
 
   updatePost() {
+    const { text } = this.state;
+    const { id, updatePostFn, hideEdit } = this.props;
 
+    updatePostFn( id, text );
+    hideEdit();
   }
 
   render() {
     // More destructuring!
     const { hideEdit } = this.props;
     const { text } = this.state;
+    
 
     return (
       <section className="Edit__parent">
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -49,11 +49,17 @@ export default class Post extends Component {
     }
   }
 
+  
+
   render() {
     // This is destructuring! You can also think of it as being written as so:
       // const editing = this.state.editing
       // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
+    const { text, date } = this.props
+    const { id, updatePostFn, hideEdit } = this.props;
+    const { deletePostFn } = this.props;
+    
 
     return (
       // Main body of post
@@ -66,8 +72,8 @@ export default class Post extends Component {
           {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
-          </div>
+            <span onClick={ () => deletePostFn( id ) }>Delete</span> { /* Remember to destructure deletePostFn off of props or use this.props.deletePostFn */ }
+        </div>
         </div>
 
         {/* This is where all the meta data of the post will go (who, when, where) */}
@@ -79,7 +85,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+        <span className="Post__date">{ date }</span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -95,10 +101,12 @@ export default class Post extends Component {
             // This has been pulled off of this.state via destructuring
             editing
             ?
-              <Edit text=""
-                    hideEdit={ this.hideEdit } />
+              <Edit text={ text }
+                    id={ this.props.id }
+                    hideEdit={ this.hideEdit }
+                    updatePostFn={ updatePostFn } />
             :
-              <span className="Post__text">POST TEXT GOES HERE</span>
+              <span className="Post__text">{ text }</span>
           }
         </div>
 
